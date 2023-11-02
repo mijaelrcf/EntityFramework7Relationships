ad4581b03f6e03ed19b7be5b0a58c07537356ea5
mijaelrcf <mijael.rcf@gmail.com>
2023-11-02 11:14:48 -0400

    Created relationships 1:1, 1:N and N:N wit SQL Server

---
+++ DTOs/CharacterDto.cs
@@ -0,0 +1,8 @@
+﻿namespace EntityFramework7Relationships.DTOs
+{
+    public record struct CharacterDto(
+        string Name, 
+        BackpackDto Backpack, 
+        List<WeaponDto> Weapons,
+        List<FactionDto> Factions);
+}
---
+++ Models/Faction.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
+
+namespace EntityFramework7Relationships.Models
+{
+    public class Faction
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        [JsonIgnore]
+        public List<Character> Characters { get; set; }
+    }
+}
